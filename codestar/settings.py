432c6df73ff51ea40d8837beeed90cec99a83a11
Samantha Atkinson <sammodeste@yahoo.com>
2024-12-30 00:42:55 +0000

    changed debug to True

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%beo_py2b=dwr)gw-+xt^_5x)^2q*r^h+cpu3&qkz0@7zec_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
