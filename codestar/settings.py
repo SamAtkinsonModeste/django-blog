e57351890a0fda5284261cd6368df80e748cd030
Samantha Atkinson <sammodeste@yahoo.com>
2025-01-04 19:38:52 +0000

    solved static files issue

--- codestar/settings.py
+++ codestar/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Directory for collected static files
@@ -139,11 +139,11 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Leave this empty or remove if unnecessary
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Media files (uploads)
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
