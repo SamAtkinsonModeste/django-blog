6cc653b0aefcdcc80ce4ad4debaa774a1bf33a86
Samantha Atkinson <sammodeste@yahoo.com>
2025-01-07 16:55:02 +0000

    created about app

---
+++ about/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+
+def about_me(request):
+    """
+    Renders the About Page
+    """
+    about = About.objects.all().order_by('-update_on').first()
+
+    return render(request, "about/about.html", {"about": about})
