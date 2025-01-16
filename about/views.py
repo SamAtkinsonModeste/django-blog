875657b6d0d7857831928b22d49e81cecc796577
Samantha Atkinson <sammodeste@yahoo.com>
2025-01-16 11:41:25 +0000

    checked code and tidyed up

--- about/views.py
+++ about/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import About
 from django.contrib import messages
+from .models import About
 from .forms import CollaborateForm
 
 # Create your views here.
@@ -8,7 +8,19 @@ from .forms import CollaborateForm
 
 def about_me(request):
     """
-    Renders the About Page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+
+    **Template**
+    :template:`about/about.html`
     """
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
